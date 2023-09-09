0781a75a62a2509901610afa7de9b4dda49eac6d
Anoop Shivayogi <anoop.shivayogi@gmail.com>
2023-09-09 16:41:15 +0530

    broadcasting data to all clients

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -21,17 +21,44 @@ function TextEditor() {
 
 
     useEffect(() => {
+      if(socket == null || quill == null) return
+
+      const handler = (delta, oldDelta, source) => {
+          quill.updateContents(delta)
+          } 
+
+      socket.on('recieve-changes', handler);
+
+      return () => {
+        socket.off('text-change', handler);
+      }
+    }, [socket, quill])
+
+
+    useEffect(() => {
         const s = io("http://localhost:3001");
         setSocket(s);
 
         return () => {
             s.disconnect();
         }
-    })
+    }, [])
 
-    // useEffect(() => {
-    //     quill.on
-    // }, [])
+    useEffect(() => {
+      if(socket == null || quill == null) return
+
+      const handler = (delta, oldDelta, source) => {
+          if(source !== 'user') return
+          socket.emit("send-changes", delta);
+          } 
+
+      quill.on('text-change', handler);
+
+      return () => {
+        quill.off('text-change', handler);
+      }
+    }, [socket, quill])
+
 
     const wrapperRef = useCallback((wrapper) => {
         if (wrapper == null) return 
@@ -39,7 +66,8 @@ function TextEditor() {
         wrapper.innerHTML = "";
         const editor = document.createElement("div");
         wrapper.append(editor);
-        new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS} })
+        const q = new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS} });
+        setQuill(q);
     }, [])
 
   return <div className="container" ref={wrapperRef}> </div>
